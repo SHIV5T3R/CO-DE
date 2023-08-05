6789ffbaeec3591ae5b26167d2c52f5205327669
Erik-Z <erikzhou7@gmail.com>
2023-08-05 10:38:00 -0400

    Added more relevant boilerplate

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,22 @@
+import os
 from argparse import ArgumentParser
-
-from flask import Flask
-
+from flask import Flask, send_from_directory
+from flask_socketio import SocketIO
 from api.routes import test
 
-
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="../frontend/dist", template_folder="../frontend/dist")
 
     app.config.from_pyfile('config.py')
     app.register_blueprint(test)
 
+    @app.route("/", defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve(path):
+        if path != "" and os.path.exists("../frontend/dist/" + path):
+            return send_from_directory("../frontend/dist", path)
+        else:
+            return send_from_directory("../frontend/dist", 'index.html')
     return app
 
 if __name__ == '__main__':
@@ -18,6 +24,19 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--port', default = 5000, type = int, help = 'port to listen on')
     args = parser.parse_args()
     port = args.port
-
+    
     app = create_app()
-    app.run(host = '0.0.0.0', port = port, debug = True, use_reloader =True)
+    socketio = SocketIO(app)
+
+    @socketio.on('connect')
+    def handle_connect():
+        print("Client connected")
+
+    @socketio.on('yjs-update')
+    def handle_update(update):
+        # Broadcast the update to all clients (except the sender)
+        socketio.emit('yjs-update', update, include_self=False)
+        print(update)
+
+
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from 'react';
+import * as Y from 'yjs';
+import { io } from 'socket.io-client';
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ydoc = useRef(new Y.Doc());
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const socket = io('http://127.0.0.1:5000');
+
+  useEffect(() => {
+    const ytext = ydoc.current.getText('shared');
+
+    // Sync Yjs text with input value
+    ytext.observe(() => {
+      const ytextValue = ytext.toString();
+      if (inputRef.current && inputRef.current.value !== ytextValue) {
+        inputRef.current.value = ytextValue;
+      }
+    });
+
+    // Listen to remote updates from the server
+    socket.on('yjs-update', (update: Uint8Array) => {
+      const byteArray = new Uint8Array(update);
+      Y.applyUpdate(ydoc.current, byteArray);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [socket]);
+
+  const handleChange = () => {
+    if (inputRef.current) {
+      const inputValue = inputRef.current.value;
+      const ytext = ydoc.current.getText('shared');
+      const ytextValue = ytext.toString();
+  
+      if (inputValue !== ytextValue) {
+        ytext.delete(0, ytext.length);
+        ytext.insert(0, inputValue);
+  
+        // Send updates to the server
+        socket.emit('yjs-update', Y.encodeStateAsUpdate(ydoc.current));
+      }
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React Flask Yjs Boilerplate</h1>
+      <input
+        ref={inputRef}
+        type="text"
+        onChange={handleChange}
+        style={{ width: '100%' }}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
