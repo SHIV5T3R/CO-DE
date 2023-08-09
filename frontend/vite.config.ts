615ccb7b656016dfa07f651c094187fd337d39c3
blackmouse572 <Ngocvlqt1995@gmail.com>
2023-08-10 00:14:58 +0700

    feat: add eslint config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,31 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/tests/setup.tsx',
+    environment: "jsdom",
+    setupFiles: "src/tests/setup.tsx",
+  },
+  resolve: {
+    alias: {
+      "@/components/*": "./src/components/*",
+      "@/components/ui/*": "./src/components/ui/*",
+      "@/components/layout/*": "./src/components/layout/*",
+      "@/lib/*": "./src/lib/*",
+      "@/hooks/*": "./src/hooks/*",
+      "@/pages/*": "./src/pages/*",
+      "@/types/*": "./types/*",
+      "@/config/*": "./src/config/*",
+      "@/styles/*": "./src/styles/*",
+      "@/utils/*": "./src/utils/*",
+      "@/assets/*": "./src/assets/*",
+      "@/public/*": "./public/*",
+      "@/store/*": "./src/store/*",
+      "@/services/*": "./src/services/*",
+      "@/*": "./*",
+    },
   },
 });
