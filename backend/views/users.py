4fb581e2ad9c5c82ce5e81a00ede0eb223dd51c9
ZakariaTalhami <zakaria.a.a.talhami@gmail.com>
2023-08-14 23:54:35 +0300

    feat: user marshmallow for serialization

--- backend/views/users.py
+++ backend/views/users.py
@@ -1,47 +1,51 @@
-from flask_restful import Resource, Api, fields, marshal_with, reqparse
+from flask_restful import Resource, Api, request
+from marshmallow_mongoengine import ModelSchema
+from marshmallow import ValidationError, fields
 
 from views.blueprints import users_bp
 from repos.users import UsersRepo
+from models.users import User
 
 users_api = Api(users_bp)
 
-user_api_model = {
-    "id": fields.String,
-    "username": fields.String,
-    "fullName": fields.String(attribute='full_name'),
-    "email": fields.String,
-    "avatar": fields.String
-}
 
-create_user_parser = reqparse.RequestParser()
-create_user_parser.add_argument('username', location='json', required=True)
-create_user_parser.add_argument('full_name', location='json', required=True)
-create_user_parser.add_argument('email', location='json', required=True)
-create_user_parser.add_argument('password', location='json', required=True)
+class UserSchema(ModelSchema):
+    class Meta:
+        model = User
+        load_only = ["password"]
+        exclude = ["is_deleted"]
 
-login_api_model = {
-    **user_api_model,
-    "access_token": fields.String,
-    "refresh_token": fields.String,
-}
 
-login_user_parser = reqparse.RequestParser()
-login_user_parser.add_argument('email', location='json', required=True)
-login_user_parser.add_argument('password', location='json', required=True)
+class LoginUserSchema(ModelSchema):
+    class Meta:
+        model = User
+        load_only = ["password"]
+        dump_only = ["username", "full_name"]
+        exclude = ["is_deleted"]
+
+    access_token = fields.Str(dump_only=True)
+    refresh_token = fields.Str(dump_only=True)
 
 
 class Users(Resource):
-    @marshal_with(user_api_model)
     def post(self):
-        user = create_user_parser.parse_args()
-        return UsersRepo.create_user(user)
+        user_serializer = UserSchema()
+        user = user_serializer.load(request.get_json())
+        return user_serializer.dump(UsersRepo.create_user(user))
 
 
 class LoginUsers(Resource):
-    @marshal_with(login_api_model)
     def post(self):
-        user = login_user_parser.parse_args()
-        return UsersRepo.login_user(user)
+        try:
+            user_serializer = LoginUserSchema()
+            user = user_serializer.load(request.get_json())
+            return user_serializer.dump(UsersRepo.login_user(user))
+        except ValidationError as e:
+            return {
+                "status": 422,
+                "error": "Invalid Input",
+                "messages": e.messages
+            }, 422
 
 
 users_api.add_resource(Users, '/')
