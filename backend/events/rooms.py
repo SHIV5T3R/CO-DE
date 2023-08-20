4f3a69df354dd729c391da095aad6bc0a6d69202
Manasseh <believemanasseh@gmail.com>
2023-08-20 18:10:43 +0100

    feat: add room creation event handler

--- backend/events/rooms.py
+++ backend/events/rooms.py
@@ -1,6 +1,7 @@
+from flask_socketio import emit
 from services.utils import socketio
 
 
 @socketio.on("create_room")
-def handle_create_room():
-    pass
+def handle_create_room(json):
+    emit("create_room_response", json)
