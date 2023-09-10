83a0c912833954273ff05ed0c34b099add0e8903
blackmouse572 <Ngocvlqt1995@gmail.com>
2023-09-10 21:09:35 +0700

    fix: move to port 3000 strict

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,15 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: true,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
