b0f2292ddb036eb324e6ca45c0997ef5bdb41b6d
Manasseh <believemanasseh@gmail.com>
2023-09-30 19:27:14 +0100

    test: add test function to test event handler to end room sessions

--- backend/tests/test_events.py
+++ backend/tests/test_events.py
@@ -3,9 +3,13 @@ import json
 from flask_socketio import SocketIOTestClient
 from constants import ImportType
 from models.projects import Project
+from models.rooms import Room
+
+ID: str
 
 
 def test_create_room(socket_client: SocketIOTestClient, project: Project):
+    global ID
     data = {
         "owner": str(project.owner.id),
         "name": "Test room",
@@ -13,6 +17,7 @@ def test_create_room(socket_client: SocketIOTestClient, project: Project):
         "project": str(project.id),
     }
     socket_client.emit("create_room", json.dumps(data))
+
     response = list(
         filter(lambda dict: dict["name"] == "create_room", socket_client.get_received())
     )[0]
@@ -26,3 +31,21 @@ def test_create_room(socket_client: SocketIOTestClient, project: Project):
     if project.import_type == ImportType.GITHUB:
         # Github projects should always be a directory
         assert project.is_directory == True
+
+    ID = str(response_data["id"])
+
+
+def test_end_room(socket_client: SocketIOTestClient):
+    data = {"id": ID}
+    socket_client.emit("end_room", json.dumps(data))
+    response = json.loads(
+        list(
+            filter(
+                lambda dict: dict["name"] == "end_room", socket_client.get_received()
+            )
+        )[0]["args"][0]
+    )
+
+    assert response["status"] == True
+
+    assert response["data"]["has_ended"] == True
