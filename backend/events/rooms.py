27beccd4b8635bd29d87dc2b5a9bdf399112b5e6
Manasseh <believemanasseh@gmail.com>
2023-11-06 06:42:20 +0100

    feat: add event handler for joining room sessions

--- backend/events/rooms.py
+++ backend/events/rooms.py
@@ -27,3 +27,14 @@ def handle_end_room(data):
         emit("end_room", schema.dumps(res))
     except ValidationError as e:
         emit("end_room", schema.dumps(e.message))
+
+
+@socketio.on("join_room")
+def handle_join_room(data):
+    try:
+        schema = UpdateRoomSchema()
+        deserialized_data = schema.loads(data, partial=True)
+        res = RoomRepo.join_room(deserialized_data)
+        emit("join_room", schema.dumps(res))
+    except ValidationError as e:
+        emit("join_room", schema.dumps(e.message))
