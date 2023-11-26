0f4ad8290c5ff90d3c23c8f20bd8662747eef8ee
Manasseh <believemanasseh@gmail.com>
2023-11-26 23:16:30 +0100

    fix: serve app with uvicorn

--- backend/start.sh
+++ backend/start.sh
@@ -2,7 +2,7 @@
 
 function start_http_server() {
     touch ../access.log && touch ../error.log
-    gunicorn --workers 3 --bind 0.0.0.0:5000 --timeout 600 --access-logfile ../access.log --error-logfile ../error.log app:app
+    gunicorn --workers 3 --bind 0.0.0.0:5000 -k uvicorn.workers.UvicornWorker --timeout 600 --access-logfile ../access.log --error-logfile ../error.log app:app
 }
 
 start_http_server
